5e4f775596e012496f07ed9fcab79658fc2f81a2
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2024-09-30 19:58:30 +0200

    circle + timings + more modes

--- components/meditateBox.tsx
+++ components/meditateBox.tsx
@@ -9,7 +9,7 @@ import {
   SelectValue
 } from './ui/select';
 
-type Modes = '4-7-8' | 'box';
+type Modes = '4-7-8' | 'box' | 'diaphragm' | 'pursed-lip' | 'resonant';
 type Actions = 'inhale' | 'hold_inhale' | 'exhale' | 'hold_exhale';
 type ActionsMap = { [action in Actions]?: number };
 
@@ -18,7 +18,10 @@ type UserActionsMap = { [action in Actions]?: UserActions };
 
 const modes: { [mode in Modes]: ActionsMap } = {
   '4-7-8': { inhale: 4, hold_inhale: 7, exhale: 8 },
-  box: { inhale: 4, hold_inhale: 4, exhale: 4, hold_exhale: 4 }
+  box: { inhale: 4, hold_inhale: 4, exhale: 4, hold_exhale: 4 },
+  diaphragm: { inhale: 4, exhale: 6 },
+  'pursed-lip': { inhale: 2, exhale: 4 },
+  resonant: { inhale: 5, exhale: 5 }
 };
 
 const dictionary: UserActionsMap = {
@@ -30,50 +33,119 @@ const dictionary: UserActionsMap = {
 
 function MeditateBox() {
   const [started, setStarted] = useState(false);
-  const [mode, setMode] = useState<'4-7-8' | 'box'>('4-7-8');
+  const [select, setSelect] = useState<Modes>('4-7-8');
+  const mode = Object.entries(modes[select]) as [Actions, number][];
+  console.log(mode);
+  const [transition, setTransition] = useState({
+    duration: mode[0]![1],
+    isFirstTime: true
+  });
 
-  const [action, setAction] = useState<'Inhale' | 'Hold' | 'Exhale'>('Inhale');
+  const defaultCounter = {
+    action: mode[0]![0],
+    timer: mode[0]![1],
+    index: 0
+  };
 
-  useEffect(() => {
-    const currentMode = modes[mode];
-    while (started) {
-      for (const [key, duration] of Object.entries(currentMode)) {
-        console.log('we here');
-        const actionKey = key as Actions;
-        const userAction = dictionary[actionKey];
-        const timer = setTimeout(() => {
-          userAction && setAction(userAction);
-        }, duration * 1000);
+  const [counter, setCounter] = useState(defaultCounter);
 
-        return () => {
-          clearTimeout(timer);
-        };
-      }
+  const updateTransitionDuration = (duration: number) => {
+    setTransition((prev) => ({ ...prev, duration }));
+  };
+
+  const updateFirstTime = (bool: boolean) => {
+    setTransition((prev) => ({ ...prev, isFirstTime: bool }));
+  };
+
+  const decrementCounterTimer = () => {
+    setCounter((prev) => ({ ...prev, timer: prev.timer - 1 }));
+  };
+
+  const handleStart = () => {
+    if (started === true) {
+      setCounter(defaultCounter);
+      updateFirstTime(true);
     }
-  }, [started, mode]);
-  const handleClick = () => {
     setStarted((prev) => !prev);
   };
+
   const handleChange = (value: string) => {
-    if (value !== '4-7-8' && value !== 'box') {
+    if (
+      value !== '4-7-8' &&
+      value !== 'box' &&
+      value !== 'diaphragm' &&
+      value !== 'pursed-lip' &&
+      value !== 'resonant'
+    ) {
       return;
     }
-    setMode(value);
+    setSelect(value);
   };
+
+  useEffect(() => {
+    if (!started) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      // just for ts to stop crying
+      if (counter.timer === undefined) {
+        return;
+      }
+
+      if (counter.timer === 0) {
+        // reset to first action
+        if (counter.index >= mode.length - 1) {
+          setCounter(defaultCounter);
+          updateTransitionDuration(defaultCounter.timer);
+          return;
+        }
+
+        // go to next action
+        const index = counter.index + 1;
+        const timer = mode[index]![1];
+        setCounter({ action: mode[index]![0], timer, index });
+        updateTransitionDuration(timer);
+
+        return;
+      }
+
+      decrementCounterTimer();
+      transition.isFirstTime && updateFirstTime(false);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [started, counter.timer]);
+
+  console.log(counter);
+
+  let isBorderBig = false;
+  const shouldBorderBeBig =
+    started &&
+    (counter.action === 'inhale' || counter.action === 'hold_inhale');
+
+  if (shouldBorderBeBig && !transition.isFirstTime) {
+    isBorderBig = true;
+  } else {
+    isBorderBig = false;
+  }
 
   return (
     <article className="relative flex h-56 w-full items-center justify-center rounded-xl border-2 border-white/15 text-xl">
       {started ? (
-        <div
-          style={{
-            transitionProperty: 'width, height',
-            transitionTimingFunction: 'ease',
-            transitionDuration: 4 + 's'
-          }}
-          className={`${started ? 'h-32 w-32' : 'h-24 w-24'} flex items-center justify-center rounded-full border-2`}
-        >
-          {action}
-        </div>
+        <>
+          <div
+            style={{
+              transitionProperty: 'transform',
+              transitionTimingFunction: 'linear',
+              transitionDuration: transition.duration + 's'
+            }}
+            className={`${isBorderBig ? 'scale-150' : 'scale-100'} absolute h-24 w-24 rounded-full border-2 border-white`}
+          />
+          <div className="flex items-center justify-center">
+            {dictionary[counter.action]}
+          </div>
+        </>
       ) : (
         'Paused'
       )}
@@ -86,11 +158,14 @@ function MeditateBox() {
           <SelectContent>
             <SelectItem value="4-7-8">4-7-8 Breathing</SelectItem>
             <SelectItem value="box">Box Breathing</SelectItem>
+            <SelectItem value="diaphragm">Diaphragmatic Breathing</SelectItem>
+            <SelectItem value="pursed-lip">Pursed Lip Breathing</SelectItem>
+            <SelectItem value="resonant">Resonant Breathing</SelectItem>
           </SelectContent>
         </Select>
 
         <button
-          onClick={handleClick}
+          onClick={handleStart}
           className="group rounded-md bg-white/5 px-3 transition-colors duration-200 ease-linear hover:bg-white/15"
         >
           <div className="rounded-xs ml-0.5 h-0 w-0 scale-75 border-b-8 border-l-[14px] border-t-8 border-b-transparent border-l-white/50 border-t-transparent transition-colors duration-200 ease-linear group-hover:border-l-white/80" />
